35512abe2bda6b18d9bd85399158620af080a39a
Sam Kennedy <kennedysam168@gmail.com>
2021-11-07 22:15:24 -0500

    passed employee test suite

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,30 +1,30 @@
-const { test } = require('@jest/globals');
-const employee = require('../lib/employee')
+const { expect } = require('@jest/globals');
+const Employee = require('../lib/employee');
 
 
-test ("name", () => {
-    
+test ("can i create a new employee", () => {
+      const employee = new Employee();
+      expect(typeof(employee)).toBe("object")
+})
+
+
+test ("can we set name from constructor", () => {
       const name = "Mike";
-      const employee = employee(name);
-
-      expect(employee.name).toEqual(name);
+      const employee = new Employee(name);
+      expect(employee.name).toBe(name);
     
 });
 
 test ("id", () => {
-   
       const id = 17;
-      const employee = employee("Mike", 17);
-
-      expect(employee.id).toEqual(id);
+      const employee = new Employee("Mike", id);
+      expect(employee.id).toBe(id);
    
 });
 
 test ("email", () => {
-    
       const email = "Mike@gmail.com";
-      const employee = employee("Mike", 17, "Mike@gmail.com");
-
-      expect(employee.email).toEqual(eamail);
+      const employee = new Employee("Mike", 17, email);
+      expect(employee.email).toBe(email);
     
 });
